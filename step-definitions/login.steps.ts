314073145860df6a9075575a43be5402726ce2f4
Anburaj <anburaj@gmail.com>
2026-03-04 18:32:47 -0800

    Update login.steps.ts

--- step-definitions/login.steps.ts
+++ step-definitions/login.steps.ts
@@ -1,6 +1,7 @@
 import { Given, When, Then } from '@wdio/cucumber-framework'
 import LoginPage from '../pageobjects/login.page'
 import { expect } from '@wdio/globals'
+import BasePage from '../pageobjects/login.page'
 
 Given('I open Amazon website', async () => {
   await LoginPage.openAmazon()
@@ -10,7 +11,7 @@ When('I click on sign in', async () => {
   await LoginPage.clickSignIn()
 })
 
-When(/^I enter invalid email "(.*)"$/, async (email: string) => {
+When(/^I enter invalid email "(.*.)"$/, async (email: string) => {
   await LoginPage.enterEmail(email)
 })
 
@@ -18,6 +19,7 @@ When('I continue login', async () => {
   await LoginPage.clickContinue()
 })
 
+//comments added
 Then('I should see login error message', async () => {
   const error = await LoginPage.getErrorText()
   await expect(error).toContain('problem')
